ad69a8762690053f9ee53e5172630b81ba87794a
AbdulmalikAlayande <alaabdulmalik03@gmail.com>
2024-08-29 23:27:56 +0100

    Testing Websocket

--- src/config/exceptions.ts
+++ src/config/exceptions.ts
@@ -1,3 +1,5 @@
+import { AetherLogger } from "./aetherLogger";
+
 export class AetherBaseException extends Error {
 	public readonly name: string;
 	public readonly details?: string;
@@ -27,15 +29,19 @@ export class SignUpFailedException extends AetherBaseException {
 	}
 }
 
+export class WebSocketConnectionFailedException extends AetherBaseException {
+	constructor(message: string, details?: string) {
+		super(message, details);
+	}
+}
 export function handleException<T>(fn: () => T): T | null {
 	try {
 		return fn();
 	} catch (error) {
 		if (error instanceof AetherBaseException) {
-			console.error(error.toString());
+			AetherLogger.error(error.toString(), error);
 			return null;
-		} else {
-			throw error;
 		}
+		throw error;
 	}
 }
